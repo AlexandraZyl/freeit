893e7360c5418f669fe5301756ec26220c9cd1da
alexa <alexandra.zyl90@mail.com>
2022-06-16 12:12:49 +0200

    Lesson 2 Type of data Java.

---
+++ part-1/src/com/zyl/homework2/TypesJava.java
@@ -0,0 +1,13 @@
+package com.zyl.homework2;
+
+public class TypesJava {
+    public static void main(String[] args) {
+        int x=3;
+        int s=5;
+        if (x>s) {
+            System.out.println("x the bigger!");
+        } else {
+            System.out.println("y the bigger! or them equal!");
+        }
+    }
+}
