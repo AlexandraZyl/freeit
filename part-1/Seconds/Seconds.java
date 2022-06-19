23cdf03cc07d4661a6fe1bec624ccdfd64088b6f
alexa <alexandra.zyl90@mail.com>
2022-06-19 10:36:12 +0200

    Lesson 3. `seconds, minute, hours`

--- part-1/Seconds/Seconds.java
+++ part-1/Seconds/Seconds.java
@@ -6,8 +6,12 @@ public class Seconds {
             int m = (s - sec) / 60;
             int min = m % 60;
             int h = (m - min) / 60;
+            int d= h/24;
+            int w= d/7
+
+            System.out.println(min + " минут " + sec + "секунд");
             System.out.println(h + " часов " + min + " минут " + sec + "секунд");
-
-
+            System.out.println(d+ "дней" + h + " часов " + min + " минут " + sec + "секунд");
+            System.out.println(w "недель" + d+ "дней" + h + " часов " + min + " минут " + sec + "секунд");
         }
 }
