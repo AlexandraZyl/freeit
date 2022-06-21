3b107ed5e670d2c1a2fae3f79c4faad57d7deaeb
alexa <alexandra.zyl90@mail.com>
2022-06-21 16:32:24 +0200

    Task9 textbook. Factorial of a number.

---
+++ part2/src/homework4Cycle/FactorialOfaNumber.java
@@ -0,0 +1,12 @@
+package homework4Cycle;
+
+/**
+ * Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а
+ * произведение, т.е. факториал числа.
+ */
+public class FactorialOfaNumber {
+    public static void main(String[] args) {
+
+
+    }
+}
